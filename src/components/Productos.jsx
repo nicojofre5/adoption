314d52dabf3fd42e38b4253e2733eb8cf2986e94
Nicolas <lnicolas.jofre@outlook.com>
2024-03-11 19:53:01 -0300

    Faltan cambiar mapeo de productos

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -24,21 +24,18 @@ function Productos() {
     request();
   }, [buscar]);
 
-  const handleChange = (event) => {
-    const value = event.target.value;
-    console.log(value);
-    setBuscar(value);
-  };
+  // const handleChange = (event) => {
+  //   const value = event.target.value;
+  //   console.log(value);
+  //   setBuscar(value);
+  // };
 
   if (loading) {
     return <div>Cargando...</div>;
   } else {
     return (
       <>
-     
-      
-       
-        {productos.slice(1, 17).map((producto) => (
+          {productos.map((producto) => (
           <Producto
             id={producto.id}
             imagen={producto.thumbnail}
